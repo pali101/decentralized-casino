917db8fe2a733b88b7a7a925d45e470b43f15043
Aashish Paliwal <aashish.paliwal@live.com>
2024-05-27 12:38:08 +0530

    modified hardhat config and updated gitignore

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+task("accounts", "Prints the list of accounts (with balance in ETH)", async (_, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address, await hre.ethers.provider.getBalance(account.address), "Wei");
+  }
+});
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
